2b715515a84a58231532c98f276f8ee1393a6c34
Brandt Walton <brandtdwalton@gmail.com>
2021-05-02 20:57:30 -0700

    Finished GUI

--- GUI.py
+++ GUI.py
@@ -6,33 +6,51 @@ from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 import serial
+import serial.tools.list_ports
 from threading import Thread
 import os
 import alicat
 from alicat import FlowController
 from PIL import ImageTk, Image
+import winsound
 
 class CPG_GUI():
     def __init__(self):
-        self.is_off_1 = True
-        self.is_off_2 = True
-        self.ser = serial.Serial('COM6', 9600)
+        ports = serial.tools.list_ports.comports()
+        for port, desc, hwid in sorted(ports):
+            if "Arduino" in desc:
+                self.port_arduino = port
+            if "Prolific" in desc:
+                self.port_alicat = port
+        self.ser = serial.Serial(self.port_arduino, 9600)
         self.ser.timeout = 1
-        self.flow_controller_air = FlowController(port='COM4', address = 'A')
-        # self.flow_controller_propane = FlowController(port='COM4', address = 'B')
-        self.flow_controller_decane = FlowController(port='COM4', address = 'C')
-        # self.flow_controller_nitrogen = FlowController(port='COM4', address = 'D')
+        self.flow_controller_air = FlowController(port=self.port_alicat, address = 'A')
+        self.flow_controller_methane = FlowController(port=self.port_alicat, address = 'B')
+        self.flow_controller_decane = FlowController(port=self.port_alicat, address = 'C')
+        self.flow_controller_nitrogen = FlowController(port=self.port_alicat, address = 'D')
+        self.pump_on = False
+        self.fan_on = False
+        self.alarm = True
+        self.disabled = False
+        self.hydrocarbon = 'Methane'
+        self.ratio = 0
     
-    def sWrite(self, x,button,is_off):
-        if is_off == True:
-            button.config(bg="#d12c2c")
-            is_off = False
-            print('nay')
+    def pWrite(self, x):
+        if self.pump_on:
+            self.pump_on = False
+            self.button_1.config(bg='#f7ed2d', text = 'Start Pump')
         else:
-            print('ye')
-            button.config(bg="#2cd137")
-            is_off = True
-        print(x)
+            self.pump_on = True
+            self.button_1.config(bg='#2cd137', text = 'Stop Pump')
+        self.ser.write(x)
+
+    def fWrite(self, x):
+        if self.fan_on:
+            self.fan_on = False
+            self.button_2.config(bg='#f7ed2d', text = 'Start Fans')
+        else:
+            self.fan_on = True
+            self.button_2.config(bg='#2cd137', text = 'Stop Fans')
         self.ser.write(x)
 
     def buildGUI(self):
@@ -42,74 +60,93 @@ class CPG_GUI():
         canvas = tk.Canvas(self.root, height=700,width=800, bg='#5c5d5e', borderwidth = 0, highlightthickness = 0)
         canvas.pack()
 
-        self.frame_left = tk.Frame(self.root, bg='#545454', borderwidth = 0)
+        self.frame_left = tk.Frame(self.root, bg='#918f8e', borderwidth = 0)
         self.frame_left.place(relwidth=0.25,relheight=0.7)
 
-        self.frame_middle = tk.Frame(self.root, bg='#545454', borderwidth = 0)
+        self.frame_middle = tk.Frame(self.root, bg='#918f8e', borderwidth = 0)
         self.frame_middle.place(relx=0.25, relwidth=0.5,relheight=0.7)
 
-        self.frame_bottom = tk.Frame(self.root, bg='#545454', borderwidth = 0)
+        self.frame_bottom = tk.Frame(self.root, bg='#918f8e', borderwidth = 0)
         self.frame_bottom.place(rely=0.7, relwidth=1, relheight=0.3)
 
-        self.frame_right = tk.Frame(self.root, bg='#545454', borderwidth = 0)
+        self.frame_right = tk.Frame(self.root, bg='#918f8e', borderwidth = 0)
         self.frame_right.place(relx=0.75, relwidth=0.25,relheight=0.7)
 
-        photoimage = ImageTk.PhotoImage(file="sdsur.png")
-        canvas.create_image(1, 1, image=photoimage)
-
-        self.label_1 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Methane flow rate (LPM)", bg='#545454')
+        self.label_1 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Methane flow rate (LPM)", bg='#918f8e')
         self.label_1.place(relx = 0.1, rely = 0.25)
 
-        self.entry_1= tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
-        self.entry_1.place(relx = 0.5, rely=0.3, relwidth=0.5,relheight=0.05, anchor = 'n')
+        self.entry_methane= tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
+        self.entry_methane.place(relx = 0.5, rely=0.3, relwidth=0.5,relheight=0.05, anchor = 'n')
 
-        self.label_2 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Eqivalence ratio", bg='#545454')
+        self.label_2 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Eqivalence ratio", bg='#918f8e')
         self.label_2.place(relx = 0.22, rely = 0.4)
 
-        self.entry_2= tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
-        self.entry_2.place(relx = 0.5, rely=0.45, relwidth=0.5,relheight=0.05, anchor = 'n')
+        self.entry_EQ= tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
+        self.entry_EQ.place(relx = 0.5, rely=0.45, relwidth=0.5,relheight=0.05, anchor = 'n')
 
-        self.label_3 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Air flow rate (LPM)", bg='#545454')
+        self.label_3 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Air flow rate (LPM)", bg='#918f8e')
         self.label_3.place(relx = 0.17, rely = 0.55)
 
-        self.label_4 = tk.Label(self.frame_left, font='Helvetica 10 bold', bg = 'light gray')
-        self.label_4.place(relx = 0.25, rely = 0.6, relwidth = 0.5)
+        self.label_air = tk.Label(self.frame_left, font='Helvetica 10 bold', bg = 'light gray')
+        self.label_air.place(relx = 0.25, rely = 0.6, relwidth = 0.5)
 
-        self.label_5 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Nitrogen to bubbler (LPM)", bg='#545454')
+        self.label_5 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="Nitrogen to bubbler (LPM)", bg='#918f8e')
         self.label_5.place(relx = 0.07, rely = 0.7)
 
-        self.label_6 = tk.Label(self.frame_middle, font='Helvetica 10 bold', bg='#545454', text = "3:00:00")
-        self.label_6.place(relx = 0.78, rely = 0.87)
+        self.label_9 = tk.Label(self.frame_left, font='Helvetica 10 bold', text="N2 Shroud flow rate (LPM)", bg='#918f8e')
+        self.label_9.place(relx = 0.1, rely = 0.1)
 
-        self.label_7 = tk.Label(self.frame_middle, font='Helvetica 10 bold', bg='#545454')
-        self.label_7.place(relx = 0.896, rely = 0.87, relwidth=0.3,relheight=0.2)
+        self.button_stop = tk.Button(self.frame_right, text="Stop All Flows", bg='#ed392f', font='Helvetica 10 bold',command = lambda: self.stopflows())
+        self.button_stop.place(relx=0.1,rely= 0.1, relheight=0.1,relwidth=0.7)
 
-        self.label_8 = tk.Label(self.frame_middle, font='Helvetica 10 bold', bg='#545454', text = "Time Remaining")
-        self.label_8.place(relx = 0.7, rely = 0.83)
+        self.button_vent = tk.Button(self.frame_right, text="Vent Chamber", bg='#ed392f', font='Helvetica 10 bold',command = lambda: self.startnitrogenshroud(40))
+        self.button_vent.place(relx=0.1,rely= 0.25, relheight=0.1,relwidth=0.7)
 
-        self.entry_5= tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
-        self.entry_5.place(relx = 0.5, rely=0.75, relwidth=0.5,relheight=0.05, anchor = 'n')
+        self.entry_N2 = tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
+        self.entry_N2.place(relx = 0.5, rely=0.15, relwidth=0.5,relheight=0.05, anchor = 'n')
 
-        self.button_1 = tk.Button(self.frame_bottom, text="Start Pump", bg='#2cd137', font='Helvetica 10 bold', command = lambda: self.sWrite(b'p',self.button_1,self.is_off_1))
-        self.button_1.place(relx=0.1,rely= 0.2, relheight=0.2,relwidth=0.2)
+        self.entry_decane = tk.Entry(self.frame_left, bg="#e0e6ef", font='Helvetica 13')
+        self.entry_decane.place(relx = 0.5, rely=0.75, relwidth=0.5,relheight=0.05, anchor = 'n')
 
-        self.button_2 = tk.Button(self.frame_bottom, text="Start Fans", bg='#2cd137', font='Helvetica 10 bold',command = lambda: self.sWrite(b'f',self.button_2,self.is_off_2))
-        self.button_2.place(relx=0.4,rely= 0.2, relheight=0.2,relwidth=0.2)
+        self.button_1 = tk.Button(self.frame_bottom, text="Start Pump", bg='#f7ed2d', font='Helvetica 10 bold', command = lambda: self.pWrite(b'p'))
+        self.button_1.place(relx=0.7,rely= 0.5, relheight=0.2,relwidth=0.2)
 
-        self.button_4 = tk.Button(self.frame_bottom, text="Start Inerts", bg='#2cd137', font='Helvetica 10 bold', command = lambda: self.startflows(self.entry_1.get(), self.Vol_air))
+        self.button_2 = tk.Button(self.frame_bottom, text="Start Fans", bg='#f7ed2d', font='Helvetica 10 bold',command = lambda: self.fWrite(b'f'))
+        self.button_2.place(relx=0.4,rely= 0.5, relheight=0.2,relwidth=0.2)
+
+        self.button_4 = tk.Button(self.frame_bottom, text="Start Nitrogen Shroud", bg='#f7ed2d', font='Helvetica 10 bold', command = lambda: [self.startnitrogenshroud(self.entry_N2.get()),
+                                                                                                                                self.button_4.config(bg='#2cd137')])
         self.button_4.place(relx=0.7,rely= 0.2, relheight=0.2,relwidth=0.2)
 
-        self.button_5 = tk.Button(self.frame_bottom, text="Stop gas flows", bg='#2cd137', font='Helvetica 10 bold', command = lambda: self.stopflows(self.entry_1.get()))
-        self.button_5.place(relx=0.7,rely= 0.5, relheight=0.2,relwidth=0.2)
+        self.button_5 = tk.Button(self.frame_bottom, text="Start Air", bg='#f7ed2d', font='Helvetica 10 bold', command = lambda: [self.startair(self.Vol_air),
+                                                                                                                                self.button_5.config(bg='#2cd137', text = 'Update Air')])
+        self.button_5.place(relx=0.1,rely= 0.2, relheight=0.2,relwidth=0.2)
 
-        self.button_5 = tk.Button(self.frame_bottom, text="Start Combustibles", bg='#2cd137', font='Helvetica 10 bold', command = lambda: self.startcombustibles(self.entry_1.get()))
-        self.button_5.place(relx=0.4,rely= 0.5, relheight=0.2,relwidth=0.2)
+        self.button_6 = tk.Button(self.frame_bottom, text="Start Methane", bg='#f7ed2d', font='Helvetica 10 bold', command = lambda: [self.startmethane(self.entry_methane.get()),
+                                                                                                self.button_6.config(bg='#2cd137', text = 'Update {}'.format(self.hydrocarbon))])
+        self.button_6.place(relx=0.4,rely= 0.2, relheight=0.2,relwidth=0.2)
+
+        self.button_3 = tk.Button(self.frame_bottom, text="Start Decane", bg='#f7ed2d', font='Helvetica 10 bold',command = lambda: [self.startdecane(self.entry_decane.get()),
+                                                                                                                                self.button_3.config(bg='#2cd137', text = 'Update Decane')])
+        self.button_3.place(relx=0.1,rely= 0.5, relheight=0.2,relwidth=0.2)
+
+        self.label_10 = tk.Label(self.frame_right, font='Helvetica 10 bold', text="Select Gas", bg='#918f8e')
+        self.label_10.place(relx = 0.25, rely = 0.55)
+
+        self.button_7 = tk.Button(self.frame_right, text="Methane", bg='#f7ed2d', font='Helvetica 10 bold',command = lambda: self.gasselect(0))
+        self.button_7.place(relx=0.1,rely= 0.6, relheight=0.1,relwidth=0.7)
+
+        self.button_8 = tk.Button(self.frame_right, text="Propane", bg='#f7ed2d', font='Helvetica 10 bold',command = lambda: self.gasselect(1))
+        self.button_8.place(relx=0.1,rely= 0.75, relheight=0.1,relwidth=0.7)
+
+        self.button_alarm = tk.Button(self.frame_right, text="Disable Alarm", bg='#2cd137', font='Helvetica 10 bold',command = lambda: self.setalarm())
+        self.button_alarm.place(relx=0.1,rely= 0.4, relheight=0.1,relwidth=0.7)
 
         self.x = [1,2,3,4,5,6,7,8,9,10]
         self.xstr =  ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null']
         self.y = np.zeros((10,), dtype=int).tolist()
 
-        self.fig = plt.figure(figsize = (4,4), dpi = 100, edgecolor= '#545454', facecolor = '#545454')
+        self.fig = plt.figure(figsize = (4,4), dpi = 100, edgecolor= '#918f8e', facecolor = '#918f8e')
         self.ax = self.fig.add_subplot(111)
         self.ax.set_ylim([0,10])
         self.temp_plot, = self.ax.plot(self.x,self.y, 'r-')
@@ -127,43 +164,87 @@ class CPG_GUI():
 
         self.update()
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.retries = 2
 
         self.root.mainloop()
     
-    def startflows(self, methane, air):
-        self.timestartflows = datetime.now()
-        self.threehourset = self.timestartflows + timedelta(hours = 3)
-        self.flow_controller_air.set_flow_rate(19200)
-        time.sleep(1)
-        # self.flow_controller_propane.set_flow_rate(3213)
+    def gasselect(self, gas):
+        if gas == 0:
+            self.flow_controller_methane.set_gas("CH4")
+            self.button_7.config(bg='#2cd137')
+            self.button_8.config(bg='#f7ed2d')
+            self.button_6.config(text='Start Methane')
+            self.hydrocarbon = 'Methane'
+            self.ratio = 9.52
+        if gas == 1:
+            self.flow_controller_methane.set_gas("C3H8")
+            self.button_8.config(bg='#2cd137')
+            self.button_7.config(bg='#f7ed2d')
+            self.button_6.config(text='Start Propane')
+            self.hydrocarbon = 'Propane'
+            self.ratio = 23.9
     
-    def startcombustibles(self, methane):
-        # self.flow_controller_nitrogen.set_flow_rate(8960)
-        self.flow_controller_decane.set_flow_rate(5120)
+    def setalarm(self):
+        if self.disabled == True:
+            self.disabled = False
+            self.button_alarm.config(bg='#2cd137', text = "Disable Alarm")
+        else:
+            self.button_alarm.config(bg='#ed392f', text = "Enable Alarm")
+            self.disabled = True
 
-    def stopflows(self, methane):
-        # self.flow_controller_nitrogen.set_flow_rate(0)
-        self.flow_controller_air.set_flow_rate(0)
-        # self.flow_controller_propane.set_flow_rate(0)
-        self.flow_controller_decane.set_flow_rate(0)
-        time.sleep(1)
+    
+    def startnitrogenshroud(self, nitrogen):
+        command = '{addr}{setpoint}\r'.format(addr="D", setpoint= int((float(nitrogen)/250)*64000))
+        self.flow_controller_nitrogen._test_controller_open()
+        self.flow_controller_nitrogen._write_and_read(command, self.retries)
+    
+    def startmethane(self, methane):
+        command = '{addr}{setpoint}\r'.format(addr="B", setpoint= int((float(methane)/5)*64000))
+        self.flow_controller_methane._test_controller_open()
+        self.flow_controller_methane._write_and_read(command, self.retries)
 
-    def update(self):
-        self.timenow2 = datetime.now()
+    def startair(self, air):
+        command = '{addr}{setpoint}\r'.format(addr="A", setpoint= int((float(air)/20)*64000))
+        self.flow_controller_air._test_controller_open()
+        self.flow_controller_air._write_and_read(command, self.retries)
+
+    def startdecane(self, decane):
+        command = '{addr}{setpoint}\r'.format(addr="C", setpoint= int((float(decane)/5)*64000))
+        self.flow_controller_decane._test_controller_open()
+        self.flow_controller_decane._write_and_read(command, self.retries)
+
+    def stopflows(self):
+        self.button_3.config(bg='#f7ed2d', text = "Start Decane")
+        self.button_6.config(bg='#f7ed2d', text = "Start {}".format(self.hydrocarbon))
+        self.button_5.config(bg='#f7ed2d', text = "Start Air")
+        self.button_4.config(bg='#f7ed2d', text = "Nitrogen Shroud")
         try:
-            self.threehourtimer = (self.threehourset - self.timenow2)
-            self.label_6.config(text = str(self.threehourtimer))
+            self.startdecane(0)
         except:
             pass
         try:
-            self.Vol_air = float(self.entry_1.get())*float(self.entry_2.get())*9.52
-            display_Vol_air = round(self.Vol_air,4)
-            if float(self.entry_2.get()) > 1:
-                self.label_4.config(text = display_Vol_air, fg = '#8f0909')
-            else:
-                self.label_4.config(text = display_Vol_air, fg = '#0e8f07')
+            self.startair(0)
         except:
-            self.label_4.config(text = 'nan', fg = '#000000')
+            pass
+        try:
+            self.startmethane(0)
+        except:
+            pass
+        try:
+            self.startnitrogenshroud(0)
+        except:
+            pass     
+
+    def update(self):
+        try:
+            self.Vol_air = float(self.entry_methane.get())*float(self.entry_EQ.get())*self.ratio
+            display_Vol_air = round(self.Vol_air,4)
+            if float(self.entry_EQ.get()) > 1:
+                self.label_air.config(text = display_Vol_air, fg = '#0e8f07')
+            else:
+                self.label_air.config(text = display_Vol_air, fg = '#8f0909')
+        except:
+            self.label_air.config(text = 'nan', fg = '#000000')
         self.root.after(1000,self.update)
 
     def plot_update(self):
@@ -186,6 +267,11 @@ class CPG_GUI():
                 data = self.ser.readline()
                 data = data[0:-2]
                 data = float(data)
+                print(data)
+                if data >= 100.0:
+                    self.alarm = True
+                if self.disabled == False and self.alarm == True and data < 100.0:
+                    self.playsound()
                 self.y.append(data)
                 self.timenow = datetime.now()
                 self.floattimedelta = self.timenow - self.timepast
@@ -193,16 +279,29 @@ class CPG_GUI():
                 self.xstr.append(self.deltatime)
                 self.xstr.pop(0)
                 self.y.pop(0)
-            # try:
-            #     self.temperature = float(self.ser.readline().decode('ascii'))
-            #     self.y.append(self.temperature)
-            #     print(self.y)
-            # except:
-            #     pass
         self.ser.close()
+
+    def playsound(self):
+        winsound.Beep(950, 1000)
     
     def on_closing(self):
         self.ser.write(b'r')
+        try:
+            self.startdecane(0)
+        except:
+            pass
+        try:
+            self.startair(0)
+        except:
+            pass
+        try:
+            self.startmethane(0)
+        except:
+            pass
+        try:
+            self.startnitrogenshroud(0)
+        except:
+            pass 
         os._exit(1)
 
 gui = CPG_GUI()
--- flowmeter.py
+++ flowmeter.py
@@ -1,6 +1,8 @@
 import alicat
-# A is air (19200), C is decane (5120), B is propane(3213), D is nitrogen (36)
-flow_controller = alicat.FlowController(port = "COM7", address = "D")
-# command = '{addr}$${setpoint:.2f}\r'.format(addr="D", setpoint=10.00)
-# flow_controller._write_and_read(command)
-print(flow_controller.get())
+import serial
+A is air (19200), C is decane (5120), B is propane(3213), D is nitrogen (36)
+flow_controller = alicat.FlowController(port = "COM4", address = "B")
+command = '{addr}{setpoint}\r'.format(addr="B", setpoint=1000.239458)
+flow_controller._test_controller_open()
+retries = 2
+line = flow_controller._write_and_read(command, retries)
